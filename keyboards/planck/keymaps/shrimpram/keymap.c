b1ccaafb86e8eeccff46952b71ca063714fd7e61
Shreeram Modi <shreerammodi10@gmail.com>
2024-01-01 06:50:26 -0800

    refactor: swap num layer enter and comma
    Placing the comma on the bottom row is too difficult to press given how
    frequently it ends up being used

--- keyboards/planck/keymaps/shrimpram/keymap.c
+++ keyboards/planck/keymaps/shrimpram/keymap.c
@@ -73,16 +73,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F5  |  F6  |  F7  |  F8  |      |  /   |  4   |  5   |  6   |  +   |  =   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F9  |  F10 |  F11 |  F12 |      |      |  1   |  2   |  3   |  *   | Enter|
+ * |      |  F9  |  F10 |  F11 |  F12 |      |      |  1   |  2   |  3   |  *   |  ,   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      |  0   |  .   |  ,   | Enter|
+ * |      |      |      |      |      |             |      |  0   |  .   | Enter| Enter|
  * `-----------------------------------------------------------------------------------'
  */
 [_NUM] = LAYOUT_planck_1x2uC(
     KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_TRNS, KC_TRNS, KC_7, KC_8,    KC_9,    KC_MINS, KC_TRNS,
     KC_TRNS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_NUM,  KC_PSLS, KC_4, KC_5,    KC_6,    KC_PPLS, KC_PEQL,
-    KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_1, KC_2,    KC_3,    KC_PAST, KC_ENT,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0, KC_PDOT, KC_COMM, KC_ENT
+    KC_TRNS, KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_1, KC_2,    KC_3,    KC_PAST, KC_COMM,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0, KC_PDOT, KC_ENT, KC_ENT
 ),
 
 /* Sym
