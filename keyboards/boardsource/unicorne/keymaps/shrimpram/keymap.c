e8e1f5dab1bce13047876e7d3327d89182943329
Shreeram Modi <smodi@smodi.net>
2024-12-10 06:23:16 -0500

    unicorne: Move L mods

--- keyboards/boardsource/unicorne/keymaps/shrimpram/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/shrimpram/keymap.c
@@ -11,15 +11,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ESC/LS │   Z   │   X   │   C   │   V   │   B   │                    │   N   │   M   │   ,   │   .   │   /   │  RSFT │
      * └───────┴───────┴───────┴───────┴───────┴───────┘                    └───────┴───────┴───────┴───────┴───────┴───────┘
      *                         ┌───────┬───────┬───────┐                    ┌───────┬───────┬───────┐
-     *                         │  ALT  │  ⌘    │ MO(1) │                    │ MO(2) │ SPACE │ ENTER │
+     *                         │ MO(1) │  ALT  │ MO(1) │                    │  SPC  │   ⌘   │ MO(2) │
      *                         └───────┴───────┴───────┘                    └───────┴───────┴───────┘
      */
 
     [0] = LAYOUT_split_3x6_3(
         LCTL_T(KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T,        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-                 TT(3), KC_A, KC_S, KC_D, KC_F, KC_G,        KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
-              LSFT_T(KC_ESC), KC_Z, KC_X, KC_C, KC_V,        KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                            KC_LALT,  KC_LGUI, MO(1),        MO(2), KC_SPC, KC_ENT
+                 TT(3), KC_A, KC_S, KC_D, KC_F, KC_G,        KC_H, KC_J, KC_K, KC_L, KC_SCLN, LCTL_T(KC_QUOT),
+              LSFT_T(KC_ESC), KC_Z, KC_X, KC_C, KC_V,        KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ESC),
+                              MO(1), KC_LALT, KC_ENT,        KC_SPC, KC_LGUI, MO(2)
     ),
 
 
