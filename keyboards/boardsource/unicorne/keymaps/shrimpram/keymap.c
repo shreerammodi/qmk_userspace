4fbac1bab18db5af46b4750b40c079b77b32fbf3
Shreeram Modi <smodi@smodi.net>
2024-12-15 17:02:56 -0500

    unicorne: update keymap

--- keyboards/boardsource/unicorne/keymaps/shrimpram/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/shrimpram/keymap.c
@@ -6,51 +6,51 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ┌───────┬───────┬───────┬───────┬───────┬───────┐                    ┌───────┬───────┬───────┬───────┬───────┬───────┐
      * │TAB/CTL│   Q   │   W   │   E   │   R   │   T   │                    │   Y   │   U   │   I   │   O   │   P   │ BSPC  │
      * ├───────┼───────┼───────┼───────┼───────┼───────┤                    ├───────┼───────┼───────┼───────┼───────┼───────┤
-     * │  TT(3)│   A   │   S   │   D   │   F   │   G   │                    │   H   │   J   │   K   │   L   │   ;   │   '   │
+     * │  MO(3)│   A   │   S   │   D   │   F   │   G   │                    │   H   │   J   │   K   │   L   │   ;   │   '   │
      * ├───────┼───────┼───────┼───────┼───────┼───────┤                    ├───────┼───────┼───────┼───────┼───────┼───────┤
-     * │ESC/LS │   Z   │   X   │   C   │   V   │   B   │                    │   N   │   M   │   ,   │   .   │   /   │  RSFT │
+     * │ESC/LS │   Z   │   X   │   C   │   V   │   B   │                    │   N   │   M   │   ,   │   .   │   /   │ESC/RS │
      * └───────┴───────┴───────┴───────┴───────┴───────┘                    └───────┴───────┴───────┴───────┴───────┴───────┘
      *                         ┌───────┬───────┬───────┐                    ┌───────┬───────┬───────┐
-     *                         │ MO(1) │   ⌘   │ MO(1) │                    │  SPC  │  ALT  │ MO(2) │
+     *                         │   ⌘   │ MO(1) │  ENT  │                    │  SPC  │ MO(2) │  ALT  │
      *                         └───────┴───────┴───────┘                    └───────┴───────┴───────┘
      */
 
     [0] = LAYOUT_split_3x6_3(
         LCTL_T(KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T,        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-                 TT(3), KC_A, KC_S, KC_D, KC_F, KC_G,        KC_H, KC_J, KC_K, KC_L, KC_SCLN, LCTL_T(KC_QUOT),
+                 MO(3), KC_A, KC_S, KC_D, KC_F, KC_G,        KC_H, KC_J, KC_K, KC_L, KC_SCLN, LCTL_T(KC_QUOT),
               LSFT_T(KC_ESC), KC_Z, KC_X, KC_C, KC_V,        KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ESC),
-                              MO(1), KC_LGUI, KC_ENT,        KC_SPC, KC_RALT, MO(2)
+                              KC_LGUI, MO(1), KC_ENT,        KC_SPC, MO(2), KC_RALT
     ),
 
 
     /*
      * ┌───────┬───────┬───────┬───────┬───────┬───────┐                    ┌───────┬───────┬───────┬───────┬───────┬───────┐
-     * │   `   │  F1   │  F2   │  F3   │  F4   │ TRNS  │                    │ TRNS  │   7   │   8   │   9   │   -   │  DEL  │
+     * │   `   │  F1   │  F2   │  F3   │  F4   │  NO   │                    │  NO   │   7   │   8   │   9   │   -   │  TRNS │
      * ├───────┼───────┼───────┼───────┼───────┼───────┤                    ├───────┼───────┼───────┼───────┼───────┼───────┤
-     * │ TRNS  │  F5   │  F6   │  F7   │  F8   │ TRNS  │                    │   *   │   4   │   5   │   6   │   =   │   +   │
+     * │ TRNS  │  F5   │  F6   │  F7   │  F8   │  NO   │                    │   *   │   4   │   5   │   6   │   =   │   +   │
      * ├───────┼───────┼───────┼───────┼───────┼───────┤                    ├───────┼───────┼───────┼───────┼───────┼───────┤
-     * │ TRNS  │  F9   │  F10  │  F11  │  F12  │ TRNS  │                    │   0   │   1   │   2   │   3   │   .   │   /   │
+     * │ TRNS  │  F9   │  F10  │  F11  │  F12  │  NO   │                    │  NO   │   1   │   2   │   3   │   .   │   /   │
      * └───────┴───────┴───────┴───────┴───────┴───────┘                    └───────┴───────┴───────┴───────┴───────┴───────┘
      *                         ┌───────┬───────┬───────┐                    ┌───────┬───────┬───────┐
-     *                         │ TRNS  │ TRNS  │ TRNS  │                    │ TRNS  │ TRNS  │ TRNS  │
+     *                         │ TRNS  │ TRNS  │ TRNS  │                    │ TRNS  │ TRNS  │   0   │
      *                         └───────┴───────┴───────┘                    └───────┴───────┴───────┘
     */
 
     [1] = LAYOUT_split_3x6_3(
-            KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_TRNS,        KC_TRNS, KC_7, KC_8, KC_9, KC_MINS, KC_DEL,
-           KC_TRNS, KC_F5, KC_F6, KC_F7, KC_F8, KC_TRNS,        KC_PAST, KC_4, KC_5, KC_6, KC_EQL, KC_PPLS,
-        KC_TRNS, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,        KC_0, KC_1, KC_2, KC_3, KC_PDOT, KC_PSLS,
-                              KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS
+            KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_NO,        KC_NO, KC_7, KC_8, KC_9, KC_MINS, KC_TRNS,
+           KC_TRNS, KC_F5, KC_F6, KC_F7, KC_F8, KC_NO,        KC_PAST, KC_4, KC_5, KC_6, KC_EQL, KC_PPLS,
+        KC_TRNS, KC_F9, KC_F10, KC_F11, KC_F12, KC_NO,        KC_NO, KC_1, KC_2, KC_3, KC_PDOT, KC_PSLS,
+                            KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_0
     ),
 
 
     /*
      * ┌───────┬───────┬───────┬───────┬───────┬───────┐                    ┌───────┬───────┬───────┬───────┬───────┬───────┐
-     * │   `   │ TRNS  │   [   │   \   │   ]   │ TRNS  │                    │ TRNS  │   &   │   *   │   -   │ TRNS  │  DEL  │
+     * │   `   │ TRNS  │   [   │   \   │   ]   │  NO   │                    │  NO   │   &   │   *   │   -   │ TRNS  │  DEL  │
      * ├───────┼───────┼───────┼───────┼───────┼───────┤                    ├───────┼───────┼───────┼───────┼───────┼───────┤
-     * │ TRNS  │ TRNS  │   (   │   _   │   )   │ TRNS  │                    │ TRNS  │   $   │   %   │   ^   │   =   │ TRNS  │
+     * │ TRNS  │ TRNS  │   (   │   _   │   )   │  NO   │                    │  NO   │   $   │   %   │   ^   │   =   │ TRNS  │
      * ├───────┼───────┼───────┼───────┼───────┼───────┤                    ├───────┼───────┼───────┼───────┼───────┼───────┤
-     * │ TRNS  │ TRNS  │   {   │   |   │   }   │ TRNS  │                    │ TRNS  │   !   │   @   │   #   │ TRNS  │ TRNS  │
+     * │ TRNS  │ TRNS  │   {   │   |   │   }   │  NO   │                    │  NO   │   !   │   @   │   #   │ TRNS  │ TRNS  │
      * └───────┴───────┴───────┴───────┴───────┴───────┘                    └───────┴───────┴───────┴───────┴───────┴───────┘
      *                         ┌───────┬───────┬───────┐                    ┌───────┬───────┬───────┐
      *                         │ TRNS  │ TRNS  │ TRNS  │                    │ TRNS  │ TRNS  │ TRNS  │
@@ -59,10 +59,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     [2] = LAYOUT_split_3x6_3(
-         KC_GRV, KC_TRNS, KC_LBRC, KC_BSLS, KC_RBRC, KC_TRNS,        KC_TRNS, KC_AMPR, KC_ASTR, KC_MINS, KC_TRNS, KC_DEL,
-        KC_TRNS, KC_TRNS, KC_LPRN, KC_UNDS, KC_RPRN, KC_TRNS,        KC_TRNS, KC_DLR,  KC_PERC, KC_CIRC, KC_EQL,  KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_LCBR, KC_PIPE, KC_RCBR, KC_TRNS,        KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_TRNS, KC_TRNS,
-                                   KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS
+         KC_GRV, KC_TRNS, KC_LBRC, KC_BSLS, KC_RBRC, KC_NO,        KC_NO, KC_AMPR, KC_ASTR, KC_MINS, KC_TRNS, KC_DEL,
+        KC_TRNS, KC_TRNS, KC_LPRN, KC_UNDS, KC_RPRN, KC_NO,        KC_NO, KC_DLR,  KC_PERC, KC_CIRC, KC_EQL,  KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_LCBR, KC_PIPE, KC_RCBR, KC_NO,        KC_NO, KC_EXLM, KC_AT,   KC_HASH, KC_TRNS, KC_TRNS,
+                                 KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     /*
@@ -79,10 +79,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [3] = LAYOUT_split_3x6_3(
-        KC_NO, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_BRMU,        KC_PGUP, KC_HOME, KC_UP, KC_END, KC_NO, QK_BOOT,
-        KC_NO, KC_NO, KC_LCTL, KC_LALT, KC_LSFT, KC_BRMD,        KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO,
-          KC_NO, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-                                   KC_NO, KC_TRNS, KC_NO,        KC_NO, KC_TRNS, KC_NO
+        QK_BOOT, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_BRMU,        KC_PGUP, KC_HOME, KC_UP, KC_END, KC_NO, KC_TRNS,
+        KC_TRNS, KC_NO, KC_LCTL, KC_LALT, KC_LSFT, KC_BRMD,        KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, KC_NO,
+          KC_TRNS, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                 KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS
     )
 
 
