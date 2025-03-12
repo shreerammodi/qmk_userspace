5067399642cb46457bff62844cdbe71bba0e989e
Shreeram Modi <smodi@smodi.net>
2025-03-12 19:41:05 -0400

    unicorne: Use TT instead of MO

--- keyboards/boardsource/unicorne/keymaps/shrimpram/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/shrimpram/keymap.c
@@ -17,9 +17,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_split_3x6_3(
           KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-           MO(3), KC_A, KC_S, KC_D, KC_F, KC_G,        KC_H, KC_J, KC_K, KC_L, KC_SCLN, LCTL_T(KC_QUOT),
+           TT(3), KC_A, KC_S, KC_D, KC_F, KC_G,        KC_H, KC_J, KC_K, KC_L, KC_SCLN, LCTL_T(KC_QUOT),
         LSFT_T(KC_ESC), KC_Z, KC_X, KC_C, KC_V,        KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_ESC),
-                        KC_LGUI, MO(1), KC_ENT,        KC_SPC, MO(2), KC_RALT
+                        KC_LGUI, TT(1), KC_ENT,        KC_SPC, TT(2), KC_RALT
     ),
 
 
